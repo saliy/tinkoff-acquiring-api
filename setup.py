2a8f7f79dbd8f6774e017124d677089a86e9930d
saliy <artur@morningstars.io>
2022-07-25 16:16:16 +0300

    add 3.6v in setup.py

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setuptools.setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
